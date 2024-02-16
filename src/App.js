4a9954770f8f1695388663147847c0461b58284b
HXXJKS <hangxuan@umich.edu>
2024-02-15 21:06:16 -0500

    IndexPage Breaks up

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <main>
+      <header>
+        <a href="/" className="logo">X-Initiatives</a>
       </header>
-    </div>
+      
+      <nav>
+        <a href="" className="nav">Login</a>
+        <a href="" className="nav">Register</a>
+      </nav>
+
+      <div className="post">
+        <div className="image">
+          <img src="https://pbs.twimg.com/profile_images/1596615125737299970/ZjeJlfUa_400x400.jpg" alt="umich logo"></img>
+        </div>
+        <div className="texts">
+          <h2>University of Michigan</h2>
+          <p>Event desecription in 20 words</p>
+        </div>
+      </div>
+
+      <div className="post">
+        <div className="image">
+          <img src="https://pbs.twimg.com/profile_images/1596615125737299970/ZjeJlfUa_400x400.jpg" alt="umich logo"></img>
+        </div>
+        <div className="texts">
+          <h2>University of Michigan</h2>
+          <p>Event desecription in 20 words</p>
+        </div>
+      </div>
+
+      <div className="post">
+        <div className="image">
+          <img src="https://pbs.twimg.com/profile_images/1596615125737299970/ZjeJlfUa_400x400.jpg" alt="umich logo"></img>
+        </div>
+        <div className="texts">
+          <h2>University of Michigan</h2>
+          <p>Event desecription in 20 words</p>
+        </div>
+      </div>
+
+    </main>
   );
 }
 
