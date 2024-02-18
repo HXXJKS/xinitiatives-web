90dd86a4b2345352b757ab31fa8e3771b0fd9f09
HXXJKS <hangxuan@umich.edu>
2024-02-18 13:03:39 -0500

    componenet design

--- src/App.js
+++ src/App.js
@@ -32,14 +32,14 @@ function App() {
         </div>
       </div>
 
-      <div className="post">
-        <div className="image">
-          <img src="https://pbs.twimg.com/profile_images/1596615125737299970/ZjeJlfUa_400x400.jpg" alt="umich logo"></img>
+      <div class="background-image">
+        <div class="logo-container">
+            <img src="logo.png" alt="Logo" class="logo" />
         </div>
-        <div className="texts">
-          <h2>University of Michigan</h2>
-          <p>Event desecription in 20 words</p>
-        </div>
+      </div>
+
+      <div className="club_description">
+        Hello there, this is Xinitiatives, a club ...
       </div>
 
     </main>
---
+++ src/pages/EventComponent.js
@@ -0,0 +1,15 @@
+function EventComponent() {
+    return (
+        <div className="post">
+            <div className="image">
+                <img src="https://pbs.twimg.com/profile_images/1596615125737299970/ZjeJlfUa_400x400.jpg" alt="umich logo"></img>
+            </div>
+            <div className="texts">
+                <h2>University of Michigan</h2>
+                <p>Event desecription in 20 words</p>
+            </div>
+        </div>
+    );
+}
+
+export default EventComponent;
